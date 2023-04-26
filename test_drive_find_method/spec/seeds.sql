87050bbb3ee6747fb6c2b094bdf3f35375298af7
George Barrett <georgeaustinbarrett@gmail.com>
2023-04-26 15:48:44 +0100

    sql seeds defined

---
+++ test_drive_find_method/spec/seeds.sql
@@ -0,0 +1,7 @@
+TRUNCATE TABLE recipes RESTART IDENTITY; -- replace with your own table name.
+
+-- Below this line there should only be `INSERT` statements.
+-- Replace these statements with your own seed data.
+
+INSERT INTO recipes (name, cooking_time, rating) VALUES ('Bangers and Mash', 45, 4);
+INSERT INTO recipes (name, cooking_time, rating) VALUES ('Fish and Chips', 30, 5);
