e8b6c23b727b39b30a5cdb3506175be4ce832c41
George Barrett <georgeaustinbarrett@gmail.com>
2023-04-26 14:21:59 +0100

    app.rb modified to implement the 'find' method for retrieving singular album data from the database

--- music_library/app.rb
+++ music_library/app.rb
@@ -7,11 +7,18 @@ DatabaseConnection.connect('music_library')
 artist_repository = ArtistRepository.new
 album_repository = AlbumRepository.new
 
-artist_repository.all.each do |artist|
- p artist
-end
+# artist_repository.all.each do |artist|
+#  p artist
+# end
 
-album_repository.all.each do |album|
-  p album
-end
+# album_repository.all.each do |album|
+#   p album
+# end
+
+# get the album with id of 3
+album = album_repository.find(6)
+
+puts album.id
+puts album.title
+puts album.release_year
 
